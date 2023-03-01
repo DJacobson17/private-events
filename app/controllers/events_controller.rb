37e6ba66f659dc3bba947313991ba947e5362dff
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2023-03-01 14:18:46 -0600

    corrected events param

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -58,6 +58,6 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :description, :date, :location)
+    params.require(:event).permit(:name, :description, :date, :location)
   end
 end
